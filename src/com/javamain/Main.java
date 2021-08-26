4f6ac93015bb19e162c5ff348c4d29a8cd893641
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-08-26 22:31:46 +0530

    Utility class has been added

--- src/com/javamain/Main.java
+++ src/com/javamain/Main.java
@@ -3,6 +3,7 @@ package com.javamain;
 import com.junitcode.DayOfWeek;
 import com.junitcode.MonthlyPayment;
 import com.junitcode.TemperatureConversion;
+import com.junitcode.Util;
 import com.junitcode.VendingMachine;
 import com.logicalcode.*;
 
@@ -25,13 +26,35 @@ public class Main {
 	
 //		StopWatch.stopWatch();
 		
-		VendingMachine.vendingMachine();
+//		VendingMachine.vendingMachine();
 		
 //		DayOfWeek.dayOfWeek(args);
 		
 //		TemperatureConversion.temperatureConversion();
 		
 //		MonthlyPayment.monthlyPayment(args);
+		
+		
+//		Utility class Programs
+		
+		
+/*		int d = Integer.parseInt(args[0]);
+		int m = Integer.parseInt(args[1]);
+		int y = Integer.parseInt(args[2]);
+		
+		int d0 = Util.dayOfWeek(d, m, y);
+		
+		System.out.println("Day of Week = "+d0);
+*/
+		
+/*		double p = Double.parseDouble(args[0]);
+		int y = Integer.parseInt(args[1]);
+		double R = Double.parseDouble(args[2]);
+		
+		double payment = Util.monthlyPayment(p, y, R);
+		
+		System.out.println("Monthly Payment = "+payment);
+*/
 	}
 
 }
---
+++ src/com/junitcode/Sqrt.java
@@ -0,0 +1,20 @@
+package com.junitcode;
+
+import java.util.Scanner;
+
+public class Sqrt {
+	public static void sqrt() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the non-negative number");
+		int c = sc.nextInt();
+		
+		double t = c;
+		
+		t = ((c/t) + t)/2;
+		
+		
+		
+		
+	}
+	
+}
